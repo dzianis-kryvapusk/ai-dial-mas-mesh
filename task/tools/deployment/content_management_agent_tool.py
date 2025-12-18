6b583eb4f83460007f1fe239eaf96e3eddeaae7c
Dzianis Kryvapusk <denis.krivopusk92@gmail.com>
2025-12-18 16:50:10 +0100

    solution

--- task/tools/deployment/content_management_agent_tool.py
+++ task/tools/deployment/content_management_agent_tool.py
@@ -5,10 +5,44 @@ from task.tools.deployment.base_agent_tool import BaseAgentTool
 
 class ContentManagementAgentTool(BaseAgentTool):
 
-    #TODO:
-    # Provide implementations of deployment_name (in core config), name, description and parameters.
-    # Don't forget to mark them as @property
-    # Parameters:
-    #   - prompt: string. Required.
-    #   - propagate_history: boolean
-    raise NotImplementedError()
+    @property
+    def deployment_name(self) -> str:
+        return "content-management-agent"
+
+    @property
+    def name(self) -> str:
+        return "content_management_agent"
+
+    @property
+    def description(self) -> str:
+        return (
+            "Agent that can work with files. Ask it when you need to:\n"
+            " - extract and analyze files content.\n"
+            " - performs RAG Search through files content."
+        )
+
+    @property
+    def parameters(self) -> dict[str, Any]:
+        return {
+            "type": "object",
+            "properties": {
+                "prompt": {
+                    "type": "string",
+                    "description": "The query or instruction to send to the Content Management Agent."
+                },
+                "propagate_history": {
+                    "type": "boolean",
+                    "default": False,
+                    "description": (
+                        "Flag to enable including the previous conversation history or not.\n"
+                        "If true - all the previous messages will be sent for context continuity.\n"
+                        "If false - send an individual message without historical context.\n"
+                        "Notes:\n"
+                        " - Only the conversation history between these two agents is shared; interactions with other agents are never included.\n"
+                        " - Should be set to `true` only when the `prompt` lacks sufficient context and the required context exists in the conversation history.")
+                },
+            },
+            "required": [
+                "prompt"
+            ]
+        }
--- task/tools/deployment/web_search_agent_tool.py
+++ task/tools/deployment/web_search_agent_tool.py
@@ -5,10 +5,40 @@ from task.tools.deployment.base_agent_tool import BaseAgentTool
 
 class WebSearchAgentTool(BaseAgentTool):
 
-    #TODO:
-    # Provide implementations of deployment_name (in core config), name, description and parameters.
-    # Don't forget to mark them as @property
-    # Parameters:
-    #   - prompt: string. Required.
-    #   - propagate_history: boolean
-    raise NotImplementedError()
+    @property
+    def deployment_name(self) -> str:
+        return "web-search-agent"
+
+    @property
+    def name(self) -> str:
+        return "web_search_agent"
+
+    @property
+    def description(self) -> str:
+        return "Agent that can performs complex web search. Ask it to find some information, verify facts, or synthesize information from multiple sources"
+
+    @property
+    def parameters(self) -> dict[str, Any]:
+        return {
+            "type": "object",
+            "properties": {
+                "prompt": {
+                    "type": "string",
+                    "description": "The query or instruction to send to the WEB Search Agent."
+                },
+                "propagate_history": {
+                    "type": "boolean",
+                    "default": False,
+                    "description": (
+                        "Flag to enable including the previous conversation history or not.\n"
+                        "If true - all the previous messages will be sent for context continuity.\n"
+                        "If false - send an individual message without historical context.\n"
+                        "Notes:\n"
+                        " - Only the conversation history between these two agents is shared; interactions with other agents are never included.\n"
+                        " - Should be set to `true` only when the `prompt` lacks sufficient context and the required context exists in the conversation history.")
+                },
+            },
+            "required": [
+                "prompt"
+            ]
+        }
